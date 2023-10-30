afb4650c0a8e91ee83a3153e729e1e43ec25403f
roldel <roldel@protonmail.com>
2023-10-30 02:14:06 +0000

    ADD room view and consumers

--- python_command.sh
+++ python_command.sh
@@ -3,3 +3,9 @@
 pip install --upgrade pip
 pip install django
 pip install -U 'channels[daphne]'
+
+#Project initialization
+cd /target
+python -m django startproject mysite
+cd mysite
+python3 manage.py startapp chat
